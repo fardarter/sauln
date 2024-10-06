426c4d16299c59e981f8f9fd6a8762e786cbb461
Saul Nachman <saulnachman@gmail.com>
2024-10-06 15:48:45 +0200

    rework: adds phdc + edits

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const sass = require('gulp-sass')(require('sass'));
 
 // a task to generate the css with sass
 gulp.task("css", function () {
