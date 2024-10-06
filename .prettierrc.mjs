572439a0a1cff0c9c9fa8b82efaea5c944d986e4
Saul Nachman <saulnachman@gmail.com>
2024-10-06 15:48:45 +0200

    rework: adds phdc + edits

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  tabWidth: 4,
+  semi: true,
+  singleQuote: false,
+};
+
+export default config;
